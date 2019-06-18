fc3b8d1717adbfa0d674c1bb72cb2b2e036cd517
MaithriK <51954929+MaithriK@users.noreply.github.com>
2019-06-18 21:13:52 +0530

    Initial commit

---
+++ app/src/main/java/com/example/titan/Where_To.java
@@ -0,0 +1,14 @@
+package com.example.titan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Where_To extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_where__to);
+    }
+}
